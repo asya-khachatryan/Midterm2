f2cf437e7417c270e0cc5c78d4e965b5c0780647
asya-khachatryan <asya_khachatryan2@edu.aua.am>
2023-03-28 10:23:42 +0400

    fixed mapping

--- app/src/main/java/com/example/midterm2/data/DataSource.kt
+++ app/src/main/java/com/example/midterm2/data/DataSource.kt
@@ -2,6 +2,10 @@ package com.example.midterm2.data
 
 class Datasource {
     suspend fun loadUsers(): List<User> {
-        return RetrofitHelper.getInstance().create(UserApiService::class.java).loadUsers()
+        return RetrofitHelper.getInstance().create(UserApiService::class.java).loadUsers().run{
+            this.map {
+                User(Name(it.name.title, it.name.first, it.name.last), it.email, it.nat)
+            }
+        }
     }
 }
